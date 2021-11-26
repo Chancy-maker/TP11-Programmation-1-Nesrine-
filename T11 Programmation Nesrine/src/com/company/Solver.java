fe80825998d2ff41984a900463e4ffefdb3c346b
Chancy-maker <71290766+Chancy-maker@users.noreply.github.com>
2021-11-26 06:45:02 +0100

    Add files via upload
    Java Docs ajouter la classe Solver

--- T11 Programmation Nesrine/src/com/company/Solver.java
+++ T11 Programmation Nesrine/src/com/company/Solver.java
@@ -5,12 +5,26 @@ import java.util.List;
 import java.util.Map;
 
 public class Solver {
+    /**
+     * Liste des bouteilles
+     */
     private ArrayList<Bottle> listBottle;
 
+
+    /**
+     * Constructeur de la classe Solver
+     * @param listBottle
+     */
     public Solver(ArrayList<Bottle> listBottle){
      this.listBottle = listBottle;
     }
 
+    /**
+     * La méthode satisfies permet de savoir si les contraintes définie sur certaines bouteille
+     * soit respecté.
+     * @param contrains
+     * @return
+     */
     public boolean satisfies(Map<Bottle, Integer> contrains){
         for (Map.Entry<Bottle, Integer> contrain : contrains.entrySet()){
             if (contrain.getKey().getUsedCapacity() != contrain.getValue()){
@@ -20,6 +34,13 @@ public class Solver {
         return true;
     }
 
+
+    /**
+     *La méthode possibleMoves() retourne la liste des mouvements potentiels dans la configuration actuelle :
+     *  vider toute bouteille non-vide, remplir toute bouteille toute bouteille non-pleine, transverser depuis
+     *  toute bouteille non-vide vers toute bouteille non-pleine.
+     * @return
+     */
    public List<Move> possibleMoves() {
         List<Move> moves = new ArrayList<>();
         for(Bottle bottle: listBottle){
@@ -41,15 +62,26 @@ public class Solver {
         return moves;
    }
 
+
+    /**
+     * La méthode reset vide chauqe bouteille de la liste des bouteilles
+     */
    public void reset(){
         for (Bottle bottle : listBottle){
             bottle.setUsedCapacity(0);
         }
    }
 
+
+    /**
+     * La méthode solver permet d'essayer tous les mouvement depuis la configuration initiale.
+     * @param contraints
+     * @return
+     */
    public Move solve(Map<Bottle, Integer> contraints){
         for (Move move : this.possibleMoves()){
             this.reset();
+            move.display();
             move.apply();
             if (this.satisfies(contraints)){
                 return move;
